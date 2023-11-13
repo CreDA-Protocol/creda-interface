c79939f61fa86533e79bebcec9e5261347508ecb
Benjamin Piette <benjaminpiette@trinity-tech.io>
2023-11-13 19:17:25 +0800

    nginx config

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,14 +5,35 @@ set -e # Stop on error
 
 APP_NAME=creda
 
+# Check if target argument is provided
+if [ -z "$1" ]; then
+  echo "Please provide a target argument. Usage: ./deployment/deploy.sh [target]"
+  exit 1
+fi
+
+TARGET=$1
+
+# Check if target argument is valid
+VALID_TARGETS=("staging" "prod")
+if [[ ! " ${VALID_TARGETS[@]} " =~ " ${TARGET} " ]]; then
+  echo "Invalid target argument. Accepted values: staging, prod"
+  exit 1
+fi
+
 # Build and copy to remote
-# TEMP npm run build
+echo "Building app"
+npm run build
+echo "Creating deployment archive file"
 tar zcvf ${APP_NAME}.tar.gz build
+echo "Uploading html/js archive to server"
 scp ${APP_NAME}.tar.gz credafront:~/
 
 # Execute remote deployment
-ssh root@credafront 'bash -s' < deployment/deploy_to_server.sh
+echo "Executing deployment to server"
+ssh root@credafront "bash -s ${TARGET}" < deployment/deploy_to_server.sh
 
 rm -f ${APP_NAME}.tar.gz
 
-echo ">>>>>> all done"
+echo "Deployment complete"
+echo "Server source code is at: /var/www/html/"
+echo "Server nginx config is at: /etc/nginx/conf.d/creda-interface.conf"
